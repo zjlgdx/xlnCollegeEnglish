6b86d0202b9e6462f9cb5f2c2cfe777470dd4020
yvan <zjlgdx@gmail.com>
2014-12-26 22:34:46 +0800

    add web project to analysis words

---
+++ Recipe5_2/WebApplication1/Scripts/analysis.js
@@ -0,0 +1,10 @@
+﻿$(function () {
+    $('button').click(function () {
+        var tables = $('table').find('td').each(function(){
+            console.log($.trim($(this).text()));
+            
+        });
+        
+
+    });
+})
